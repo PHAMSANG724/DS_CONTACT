0210e6deeb0e490cb180ecda32cea8d64e19c936
PSang <phamsang141200@gmail.com>
2022-10-25 08:22:23 +0700

    Ds_Contact

---
+++ app/src/main/java/com/example/ds_contact/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.ds_contact;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnGetConPressed(View v) {
+        getPhoneContacts();
+    }
+
+    private void getPhoneContacts() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 0);
+        }
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        Log.i("CONTACT_PROVIDER_DEMO", "Total # of Contacts ::: " + (cursor.getCount()));
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String contactName = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String contactNumber = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                Log.i("CONTACT_PROVIDER_DEMO", "Contact Name  ::: " + contactName + "Ph # :::" + contactNumber);
+            }
+        }
+    }
+}
